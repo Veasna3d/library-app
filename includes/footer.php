3fc4c97d995c2ec20773667c5091697c4acf6862
Khemra <makaral561@gmail.com>
2022-08-15 21:54:20 +0700

    Delete include.php

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="main-footer">
-    <div class="container">
-      <div class="pull-right hidden-xs">
-        <b>All rights reserved</b>
-      </div>
-      <strong>Copyright &copy; 2018 <a href="https://www.sourcecodester.com/">SourceCodeSter</a></strong>
-    </div>
-    <!-- /.container -->
-</footer>
